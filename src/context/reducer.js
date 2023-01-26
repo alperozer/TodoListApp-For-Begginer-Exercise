7515523facdfbb565324266f39b9ae10255f231b
Alper Özer <alperozer009@gmail.com>
2023-01-26 11:17:14 +0300

    forBerke

---
+++ src/context/reducer.js
@@ -0,0 +1,22 @@
+export const initialState={
+    todos:[
+        {
+            id:0,
+            context:'ben ilk todoyum',
+        },
+    ],
+};
+const reducer =(state ,action)=>{
+    console.log(action);
+    switch(action.type){
+        case "ADD_TODO":
+        return {
+            ...state
+        };
+        default:
+            return {
+                ...state
+            };
+    }
+}
+export default reducer;
